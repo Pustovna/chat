222c7bd50d54b35d5f80efb333ec80f4792ce511
Anastasiya <pustoe0@gmail.com>
2022-03-12 23:02:43 +0300

    chat has been finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import "./App.css";
 import AppRouter from "./components/AppRouter";
+import Loader from "./components/Loader";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Context } from "./index";
 
 const App = () => {
+  const { auth } = useContext(Context);
+  const [user, loading, error] = useAuthState(auth);
+
+  if (loading) {
+    return <Loader />;
+  }
+
   return (
     <BrowserRouter>
       <Navbar />
--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,8 +1,21 @@
 import React from "react";
+import { Button, Container, Grid } from "@mui/material";
 
 const Loader = () => {
-  return <div></div>;
+  return (
+    <Container>
+      <Grid
+        container
+        style={{ height: window.innerHeight - 50 }}
+        alugnItems={"center"}
+        justify={"center"}
+      >
+        <Grid container alignItems={"center"} direction={"column"}>
+          <div className="lds-hourglass"></div>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
 
 export default Loader;
-
