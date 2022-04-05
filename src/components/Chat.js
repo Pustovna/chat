229742a66b27f485b76e727bb9a0140978cc2253
Anastasiya <pustoe0@gmail.com>
2022-04-05 20:44:27 +0300

    added delete function

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,23 +1,34 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { Avatar, Button, Container, Grid } from "@mui/material";
+import { Avatar, Button, Container, Grid, IconButton } from "@mui/material";
 import { Context } from "../index";
 import { TextField } from "@mui/material";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 import Loader from "./Loader";
 
 import firebase from "firebase/compat/app";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, doc, deleteDoc, collectionGroup, onSnapshot, connectFirestoreEmulator } from "firebase/firestore";
 
 const Chat = () => {
   const { auth, firestore } = useContext(Context);
   const [user] = useAuthState(auth);
   const [value, setValue] = useState("");
-  const [messages, loading] = useCollectionData(
-    firestore.collection("messages").orderBy("createAt")
+  // const [messages, loading] = useCollectionData(
+  //   firestore.collection("messages").orderBy("createAt")
+  // );
+  const [messages, setMesssages] = useState([])
+  
+
+  useEffect(
+    () => 
+    onSnapshot(collection(firestore, "messages"), (snapshot) => 
+      setMesssages(snapshot.docs.map((doc) => ({...doc.data(), id: doc.id})))
+    ),
+    []
   );
 
   const sendMessage = async () => {
+    console.log('sendMessage')
     if (value !== "") {
       firebaseSendMessage();
       setValue("");
@@ -36,22 +47,43 @@ const Chat = () => {
     });
   };
 
-  const firebaseGetData = async () => {
-    console.log("start");
-    const querySnapshot = await getDocs(collection(firestore, "messages"));
-    console.log(querySnapshot);
-    querySnapshot.forEach((doc) => {
-      // doc.data() is never undefined for query doc snapshots
-      console.log(doc.data().text);
-    });
-    console.log("stop");
+  const deleteMessage = async (e, id) => {
+    // const del = await deleteDoc(doc(firestore, "messages", "14iqDptS1g0LHmCvTelJ"));
+
+    // const del = await collectionGroup(firestore, "messages");
+    // console.log(firestore.collection('messages').docs);
+    // const item = await onSnapshot(collection(firestore, "messages"), (snapshot) => {
+    //   snapshot.docs.map((doc) => ({...doc.data(), id: doc.id}))
+    // })
+    // const idMessage = await e.target.getAttribute('iddb');
+    // const idMessage = await this.getAttribute('iddb');
+    // console.log(iddb);
+    // 
+    // console.log(messages.length)
+
+    if (id) {
+      deleteDoc(doc(firestore, "messages", id));
+    } else {
+      console.log('id not found')
+    }
   };
 
-  firebaseGetData();
 
-  if (loading) {
-    return <Loader />;
-  }
+  const firebaseGetData = async () => {
+    const querySnapshot = await getDocs(collection(firestore, "messages"));
+    const data = [];
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      const newItem = {data: doc.data(), id: doc.id}
+     data.push(newItem);
+    });
+
+    return data;
+  };
+
+  // if (loading) {
+  //   return <Loader />;
+  // }
 
   return (
     <Container>
@@ -68,15 +100,15 @@ const Chat = () => {
             overflowY: "auto",
           }}
         >
-          {messages.map((message, index) => (
+          {messages.map((message) => (
             <div
-              key={index}
+              key={message.id}
               style={{
                 margin: 10,
                 border:
                   user.uid === message.uid
-                    ? "2px solid green"
-                    : "2px dashed red",
+                    ? "1px solid green"
+                    : "1px dashed red",
                 marginLeft: user.uid === message.uid ? "auto" : "10px",
                 width: "fit-content",
                 padding: 5,
@@ -84,7 +116,16 @@ const Chat = () => {
             >
               <Grid container>
                 <Avatar src={message.photoURL}></Avatar>
-                <div>{message.text}</div>
+                <div  >{message.text}</div>
+                <IconButton onClick={(e) => {deleteMessage(e, message.id)}}>
+                  <div
+                    style={{
+                      fontSize: "10px",
+                    }}
+                  >
+                    delete
+                  </div>
+                </IconButton>
               </Grid>
             </div>
           ))}
@@ -111,3 +152,4 @@ const Chat = () => {
 };
 
 export default Chat;
+
